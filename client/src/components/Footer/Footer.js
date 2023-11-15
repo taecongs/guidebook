df2f52c37ff96cb6c3e3643dd6b91c7ac741c18e
taegongs <bks0927w@gmail.com>
2023-11-15 14:52:56 +0900

    Footer 수정

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,29 @@
+import { Link } from "react-router-dom";
 import './Footer.css';
 
 const Footer = () => {
     return(
         <footer>
-            <h1 class="logo">
+            <h1 class="footer-logo">
                 <img src="./image/ft_logo.png" alt="footer" />
+
+                <div class="sidemap">
+                    <Link to="" class="sm-1">
+                        <span>포켓몬 도감</span>
+                    </Link>
+
+                    <span class="sm-line">|</span>
+
+                    <Link to="" class="sm-2">
+                        <span>포켓몬 등록</span>
+                    </Link>
+                </div>
             </h1>
+            
 
             <div class="copy">
-                <p>Copyright ©2023, BANG is Pokémon Guidebook Project All Rights Reserved. <span class="lu-txt">Latest Updated  2023년 12월 31일</span></p>
+                <p class="copy-txt">Copyright ©2023, BANG is Pokémon Guidebook Project All Rights Reserved.</p>
+                {/* <p>Copyright ©2023, BANG is Pokémon Guidebook Project All Rights Reserved. <span class="lu-txt">Latest Updated  2023년 12월 31일</span></p> */}
             </div>
         </footer>
     )
