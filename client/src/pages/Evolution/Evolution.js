396b2697daccad3b66d8819f6533265f45beb218
taegongs <bks0927w@gmail.com>
2023-12-01 10:48:58 +0900

    Evolution.js 기본적인 세팅

--- client/src/pages/Evolution/Evolution.js
+++ client/src/pages/Evolution/Evolution.js
@@ -1,7 +1,15 @@
+import './Evolution.css';
+
 const Evolution = () => {
     return(
         <section>
-            진화
+            <div className="evolution-container">
+                <div className="evolution-wrap">
+                    <div className="evolution-content">
+                        <h2 class="evolution-content-tit">포켓몬 진화</h2>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
