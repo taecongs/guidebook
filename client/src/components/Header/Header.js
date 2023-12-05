41547880e13b5251980bdc9e69aef04c07f6c510
taegongs <bks0927w@gmail.com>
2023-12-05 18:02:15 +0900

    [헤더] 새로고침 시 이미지 엑박 뜨는 문제로 이미지 경로 수정

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -13,28 +13,29 @@ const Header = () => {
     return(
         <header>
             <h1 className="logo" onClick={handleLogoClick}>
-                <Link to="/"><img src="./image/logo.png" alt="home" /></Link>
+                <Link to="/"><img src={process.env.PUBLIC_URL + '/image/logo.png'} alt="home" /></Link>
             </h1>
 
             <div className="nav">
 			    <ul className="acc">
 			    	<li>
                         <Link to="/" className="acc_a">
-                            <img src="./image/icon_ball_c.png" alt="guidebook" />
+                            <img src={process.env.PUBLIC_URL + '/image/icon_ball_c.png'} alt="포켓몬 도감 아이콘" />
                             <span className="acc_tit">포켓몬 도감</span>
                         </Link>
 			    	</li>
     
 			    	<li>
 			    		<Link to="/registration" className="acc_a">
-                            <img src="./image/icon_register.png" alt="guidebook" />
+                            <img src={process.env.PUBLIC_URL + '/image/icon_register.png'} alt="포켓몬 등록 아이콘" />
                             <span className="acc_tit">포켓몬 등록</span>
                         </Link>
 			    	</li>
 
                     <li>
 			    		<Link to="/evolution" className="acc_a">
-                            <img src="./image/icon_evo.png" alt="guidebook" />
+                            <img src={process.env.PUBLIC_URL + '/image/icon_evo.png'} alt="포켓몬 진화 아이콘" />
+                            {/* <img src="./image/icon_evo.png" alt="guidebook" /> */}
                             <span className="acc_tit">포켓몬 진화</span>
                         </Link>
 			    	</li>
@@ -42,7 +43,7 @@ const Header = () => {
             </div>
 
             <Link to="https://www.pokemonstore.co.kr/" target="_blank" className="btn-site">
-                <img className="site-img" src="./image/site2.png" alt="site" />
+                <img className="site-img" src={process.env.PUBLIC_URL + '/image/site2.png'} alt="site" />
             </Link>
         </header>
     )
