b803b16d0450db9ee1f3fb662cbce664d4ad8622
taegongs <bks0927w@gmail.com>
2023-12-21 18:42:08 +0900

    상태 관리 코드 수정

--- client/src/atom/pokemonState.js
+++ client/src/atom/pokemonState.js
@@ -1,14 +1,20 @@
 import {atom} from 'recoil';
 
 
-// 검색 한 포켓몬 데이터 상태를 저장
+// 검색한 포켓몬 데이터를 저장한다.
 export const pokemonState = atom({
     key: 'pokemonState',
     default: []
 });
 
-// 검색어 상태를 저장
+// 검색어를 저장한다.
 export const searchTextState = atom({
     key: 'searchTextState',
-    default: '',
+    default: ''
 });
+
+// 검색 결과가 없는 경우를 나타낸다.
+export const noResultsState = atom({
+    key: 'noResultsState',
+    default: false
+})
